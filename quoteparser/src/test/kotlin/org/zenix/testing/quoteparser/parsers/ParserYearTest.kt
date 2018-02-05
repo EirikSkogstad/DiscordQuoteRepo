10977a86d1a9f078558a2c8e6e6a06583e04cddf
Eirik Skogstad <eiriskogstad13@gmail.com>
2018-02-05 20:46:46 +0100

    Fixed kotlin version and expanded test

--- quoteparser/src/test/kotlin/org/zenix/testing/quoteparser/parsers/ParserYearTest.kt
+++ quoteparser/src/test/kotlin/org/zenix/testing/quoteparser/parsers/ParserYearTest.kt
@@ -3,11 +3,21 @@ package org.zenix.testing.quoteparser.parsers
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
+import org.zenix.testing.quoteparser.models.Quote
 
 class ParserYearTest {
     private val parserYear: ParserYear = ParserYear()
-    private val validQuotes: Array<String> = arrayOf("han stjæler jo bare ÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆHHHHHH\n -Eirik 2017")
-    private val invalidQuotes: Array<String> = arrayOf("\"Anders, jeg ser for meg morra di ute på alle fire og tygger på internettkabelen\" - Bergo 29.01.2018", "Not unbearable is what we strive for here at riot games :ok_hand::skin-tone-1:\" ~Riot Phreak 26/09/2017")
+    private val validQuotes: Array<String> = arrayOf(
+            "han stjæler jo bare ÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆHHHHHH\n -Eirik 2017",
+            "\"Billy Motlem\" - Sloth 2018")
+    private val invalidQuotes: Array<String> = arrayOf(
+            "\"Anders, jeg ser for meg morra di ute på alle fire og tygger på internettkabelen\" - Bergo 29.01.2018",
+            "Not unbearable is what we strive for here at riot games :ok_hand::skin-tone-1:\" ~Riot Phreak 26/09/2017")
+
+    private val expectedResults: Array<Quote> = arrayOf(
+            Quote("Eirik", "han stjæler jo bare ÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆHHHHHH", "2017"),
+            Quote("Sloth", "Billy Motlem", "2018")
+    )
 
     @Test
     fun isCorrectFormat() {
@@ -21,13 +31,14 @@ class ParserYearTest {
 
     @Test
     fun parse() {
-        for (validQuote in validQuotes) {
+        for ((i, validQuote) in validQuotes.withIndex()) {
             try {
                 val (author, text, date) = parserYear.parse(validQuote)
 
-                assertEquals("Eirik", author)
-                assertEquals("2017", date)
-                assertEquals("han stjæler jo bare ÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆHHHHHH", text)
+                val expectedQuote = expectedResults[i]
+                assertEquals(expectedQuote.author, author)
+                assertEquals(expectedQuote.text, text)
+                assertEquals(expectedQuote.date, date)
             } catch (e: QuoteParsingException) {
                 fail("Could not parse " + validQuote)
             }
